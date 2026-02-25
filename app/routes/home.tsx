dbaa66b64618801d6cb8ad99dfd5f0dcef40fd82
Ajit <dasajit697@gmail.com>
2026-02-25 22:06:16 +0600

    Add ResumeCard and ScoreCircle components, and define Resume type with sample data

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,9 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 import Navbar from "~/components/Navbar";
+import { resumes } from "~/constants";
+import { resume } from "react-dom/server";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,10 +16,20 @@ export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
       <section className="main-section">
-           <div className="page-heading">
+           <div className="page-heading py-16">
                <h1>Track Your Applications & Resume Ratings</h1>
                <h2>Get AI-powered insights to improve your job applications</h2>
            </div>
       </section>
+
+
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+            {resumes.map(resume => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+        </div>
+      )}
+    
   </main>
 }
